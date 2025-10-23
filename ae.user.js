825b986162f1e9d2f28bd04f0fcf0e1a8620e5f7
h-maruta-ai <h-maruta@ilab.pu-kumamoto.ac.jp>
2025-10-23 22:01:51 +0900

    Update ae.user.js

--- ae.user.js
+++ ae.user.js
@@ -29,8 +29,9 @@
             align-items: center;
             box-shadow: 0 -2px 8px rgba(0,0,0,0.1);
         ;
+
         const img = document.createElement('img');
-        img.src = 'https://raw.githubusercontent.com/h-maruta-ai/abroad_experience/701208ac571d59566127812d5f93f11fa5040ab5/abroadex.png';
+        img.src = 'https://raw.githubusercontent.com/h-maruta-ai/abroad_experience/b12d1c759d01c7c3126ca70b0233e8453a33f1e5/abroadex.png';
         img.alt = '広告バナー';
         img.style.cssText = 
             max-width: 700px;
